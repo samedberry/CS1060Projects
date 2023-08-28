cc1d86a749fe46dd0d9e58166863386b4eb9ce96
samedberry <143440194+samedberry@users.noreply.github.com>
2023-08-28 17:53:46 -0600

    cleanup comments
    moved all default comments to top of file

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+// Press Alt+Enter with your caret at the highlighted text to see how
+// IntelliJ IDEA suggests fixing it.
+
+// Press Shift+F10 or click the green arrow button in the gutter to run the code.]
+
+// Press Shift+F9 to start debugging your code. We have set one breakpoint
+// for you, but you can always add more by pressing Ctrl+F8.
+
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        System.out.printf("Hello and welcome!\n");
         for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
