84cb7a134e4da5456b61ed6545c86ed8ac4fabfa
samedberry <143440194+samedberry@users.noreply.github.com>
2023-09-13 17:51:14 -0600

    Update GradeCalculator
    started fleshing out logic in comments

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -17,17 +17,17 @@ For each one, output the letter grade corresponding to the numeric grade.
 If the grade entered is less than 0, print an error message.
 
 the ranges are listed on the syllabus as:
-    [92, ∞): A
-    [89, 92): A-
-    [87, 89): B+
-    [82, 87): B
-    [79, 82): B-
-    [77, 79): C+
-    [72, 77): C
-    [69, 72): C-
-    [67, 69): D+
-    [60, 67): D
-    [0, 60): F
+    [92, ∞): A              if gradePercent >= 92
+    [89, 92): A-            if gradePercent >= 89 && gradePercent < 92
+    [87, 89): B+            if gradePercent >= 87 && gradePercent < 89
+    [82, 87): B             if gradePercent >= 82 && gradePercent < 87
+    [79, 82): B-            if gradePercent >= 79 && gradePercent < 82
+    [77, 79): C+            if gradePercent >= 77 && gradePercent < 79
+    [72, 77): C             if gradePercent >= 72 && gradePercent < 77
+    [69, 72): C-            if gradePercent >= 69 && gradePercent < 72
+    [67, 69): D+            if gradePercent >= 67 && gradePercent < 69
+    [60, 67): D             if gradePercent >= 60 && gradePercent < 67
+    [0, 60): F              if gradePercent >= 0 && gradePercent < 60
 */
 
         String gradeLetter = "test";
