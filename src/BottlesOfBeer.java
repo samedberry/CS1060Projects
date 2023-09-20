ad6ccf295b258e2fd3d7a80622c58620d58eb936
samedberry <143440194+samedberry@users.noreply.github.com>
2023-09-20 17:03:27 -0600

    revision BottlesOfBeer
    revision to BottlesOfBeer based on Ryan's feedback

--- src/BottlesOfBeer.java
+++ src/BottlesOfBeer.java
@@ -12,22 +12,31 @@ public class BottlesOfBeer {
         System.out.print("Enter the number of bottles to start with: ");
         int bottlesInitial = input1.nextInt();
 
-        //start singing from input number of bottles and loop decrementally
+        //start singing from input number of bottles if more than 2 and loop decrementally
         for (int bottles = bottlesInitial; bottles > 2; bottles--){
             System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
             System.out.println("Take one down and pass it around, " + (bottles-1) + " bottles of beer on the wall.");
             System.out.println();
         }
 
-        //finish the song with correct english
-        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
-        System.out.println("Take one down and pass it around, 1 bottle of beer on the wall.");
-        System.out.println();
-        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
-        System.out.println("Take one down and pass it around, 0 bottles of beer on the wall.");
-        System.out.println();
-        System.out.println("0 bottles of beer on the wall, 0 bottles of beer.");
+        //grammatically correct verse for 2 bottles if necessary
+        if (bottlesInitial >= 2){
+            System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
+            System.out.println("Take one down and pass it around, 1 bottle of beer on the wall.");
+            System.out.println();
 
+        }
+
+        //gramatically correct verse for 1 bottle if necessary
+        if (bottlesInitial >= 1){
+            System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+            System.out.println("Take one down and pass it around, 0 bottles of beer on the wall.");
+            System.out.println();
+
+        }
+
+        //gramatically correct finish of song
+        System.out.println("0 bottles of beer on the wall, 0 bottles of beer.");
         //callback to how many you started with
         System.out.println("Go to the store and buy some more, " + bottlesInitial + " bottles of beer on the wall.");
         System.out.println();
