07d360e560976ab854feb4e40a21456fd39511d5
samedberry <143440194+samedberry@users.noreply.github.com>
2023-10-27 17:51:45 -0600

    scratchwork from class

--- src/ScratchWork.java
+++ src/ScratchWork.java
@@ -1,8 +1,5 @@
 public class ScratchWork {
     public static void main(String[] args) {
-        String timestamp = "4:15:56";
-//        Hint: you'll want to use `String.split(":")`
-            System.out.printf("%02d", Integer.parseInt(timestamp.split(":")[0]));
-//        Hint: you'll probably also want to use `Integer.parseInt`
+
         }
 }
