4574c428e453212545a6c84c03353b998d6fc822
samedberry <143440194+samedberry@users.noreply.github.com>
2023-12-01 11:57:32 -0700

    Started TotalCalculation

---
+++ src/TotalCalculation.java
@@ -0,0 +1,17 @@
+//    Open the “items.txt” file.
+//    Read all of the values in a loop. You will need to use “hasNextDouble” for this.
+//    Keep a running total of the numbers in the file.
+//    Calculate the sub-total,
+//    use 5.3% tax to calculate final total.
+//    Write sub-total, tax, and final total to the “total.txt” file.
+//    Print only two decimal points as these are monetary values.
+
+//Sample Output:
+// The sub-total is $72.04
+//The tax is $3.82
+//The total is $75.86
+
+
+
+public class TotalCalculation {
+}
