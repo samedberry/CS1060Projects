2d864f8f00e5da363f36649d25d0888ddf621d24
samedberry <143440194+samedberry@users.noreply.github.com>
2023-12-05 11:55:41 -0700

    Finished TotalCalculation

--- src/ScratchWork.java
+++ src/ScratchWork.java
@@ -1,7 +1,6 @@
 //FiguringOutMyGrade
 //in class exercise
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -9,18 +8,13 @@ import java.util.Scanner;
 public class ScratchWork {
     public static void main(String[] args) {
 
-
-        File file = new File("grades.csv");
-
-        FileReader fileReader = null;
-        try{
-            fileReader = new FileReader(file);
+        try (Scanner input1 = new Scanner(new FileReader("grades.csv"))){
+            System.out.println(input1.nextLine());
         } catch(FileNotFoundException e){
             System.out.println("File Not Found");
         }
-        Scanner input1 = new Scanner(fileReader);
 
-        System.out.println(input1.nextInt());
+//        System.out.println(input1.nextInt());
 
     }
 }
--- src/TotalCalculation.java
+++ src/TotalCalculation.java
@@ -1,32 +1,29 @@
-//    Open the “items.txt” file.
-//    Read all of the values in a loop. You will need to use “hasNextDouble” for this.
-//    Keep a running total of the numbers in the file.
-//    Calculate the sub-total,
-//    use 5.3% tax to calculate final total.
-//    Write sub-total, tax, and final total to the “total.txt” file.
-//    Print only two decimal points as these are monetary values.
-
-//Sample Output:
-// The sub-total is $72.04
-//The tax is $3.82
-//The total is $75.86
-
-
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class TotalCalculation {
     public static void main(String[] args) throws Exception {
-
-        System.out.println();
-
         File file = new File("items.txt");
         FileReader fileReader = new FileReader(file);
-        System.out.println();
-//        Scanner input1 = new Scanner(fileReader);
-//
-//        System.out.println(input1.nextDouble());
-    }
+        Scanner input1 = new Scanner(fileReader);
 
+        double subtotal = 0.0;
+        while (input1.hasNextDouble()) {
+            subtotal = subtotal + input1.nextDouble();
+        }
+
+        double tax = subtotal * .053;
+        double total = subtotal + tax;
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        try (FileWriter fileWriter = new FileWriter("total.txt")) {
+            fileWriter.write("The sub-total is $" + df.format(subtotal) + "\n");
+            fileWriter.write("The tax is $" + df.format(tax) + "\n");
+            fileWriter.write("The total is $" + df.format(total));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
