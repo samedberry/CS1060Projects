9e2742b154b21ae7d26faf900ea58357fa2e81ab
samedberry <143440194+samedberry@users.noreply.github.com>
2023-12-04 18:04:58 -0700

    scratchwork in class

--- src/ScratchWork.java
+++ src/ScratchWork.java
@@ -1,7 +1,26 @@
-import java.io.FileWriter;
+//FiguringOutMyGrade
+//in class exercise
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class ScratchWork {
     public static void main(String[] args) {
-//        FileWriter fileWriter = new FileWriter();
+
+
+        File file = new File("grades.csv");
+
+        FileReader fileReader = null;
+        try{
+            fileReader = new FileReader(file);
+        } catch(FileNotFoundException e){
+            System.out.println("File Not Found");
+        }
+        Scanner input1 = new Scanner(fileReader);
+
+        System.out.println(input1.nextInt());
+
     }
 }
