d8cd1b0bfbc63afcd69cbb5c6675a6dc14d42ad0
samedberry <143440194+samedberry@users.noreply.github.com>
2024-01-18 13:53:49 -0700

    Scratch work
    commented old for function, added Spring

--- src/ScratchWork.java
+++ src/ScratchWork.java
@@ -20,29 +20,29 @@ public class ScratchWork {
         List<Integer> grades = new ArrayList<>();
         String classNumber = "";
 
-        while (sc.hasNext()) {
-            String line = sc.nextLine();
-            if (!line.substring(line.length() - 1).equals("W")) {
-                int firstComma = line.indexOf(",");
-                String noClassName = line.substring(firstComma + 1);
-                int secondComma = noClassName.indexOf(",");
-                String noClassNumber = noClassName.substring(secondComma + 1);
-                String gradeAsString = noClassNumber.substring(0, noClassNumber.indexOf(","));
-                if (classNumber.equals(noClassName.substring(0, secondComma)) && grades.get(grades.size() - 1) < Integer.parseInt(gradeAsString)) {
-                    classNumber = noClassName.substring(0, secondComma);
-                    grades.set(grades.size() - 1, (Integer.parseInt(gradeAsString)));
-                } else if (gradeAsString) {
-
-                }else {
-                    classNumber = noClassName.substring(0, secondComma);
-                    try {
-                        grades.add(Integer.parseInt(gradeAsString));
-                    } catch (NumberFormatException e) {
-                        //convert letter to number and add to list
-                    }
-                }
-            }
-        }
+//        while (sc.hasNext()) {
+//            String line = sc.nextLine();
+//            if (!line.substring(line.length() - 1).equals("W")) {
+//                int firstComma = line.indexOf(",");
+//                String noClassName = line.substring(firstComma + 1);
+//                int secondComma = noClassName.indexOf(",");
+//                String noClassNumber = noClassName.substring(secondComma + 1);
+//                String gradeAsString = noClassNumber.substring(0, noClassNumber.indexOf(","));
+//                if (classNumber.equals(noClassName.substring(0, secondComma)) && grades.get(grades.size() - 1) < Integer.parseInt(gradeAsString)) {
+//                    classNumber = noClassName.substring(0, secondComma);
+//                    grades.set(grades.size() - 1, (Integer.parseInt(gradeAsString)));
+//                } else if (gradeAsString) {
+//
+//                }else {
+//                    classNumber = noClassName.substring(0, secondComma);
+//                    try {
+//                        grades.add(Integer.parseInt(gradeAsString));
+//                    } catch (NumberFormatException e) {
+//                        //convert letter to number and add to list
+//                    }
+//                }
+//            }
+//        }
 
         int sum = 0;
         for (int i : grades) {
