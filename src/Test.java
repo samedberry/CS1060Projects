aa711d090a007b2742de2b2f47bcb01539ca57e5
samedberry <143440194+samedberry@users.noreply.github.com>
2024-01-21 20:15:47 -0700

    scratch work for quiz

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(-1%4);
-        System.out.println(0%4);
-        System.out.println(1%4);
-        System.out.println(2%4);
-        System.out.println(3%4);
-        System.out.println(4%4);
+        System.out.println((double)(5/2));
     }
 }
