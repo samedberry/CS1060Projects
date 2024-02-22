c06904c880eeac25c684bc2a8443bb136411b8bc
samedberry <143440194+samedberry@users.noreply.github.com>
2024-02-22 13:28:05 -0700

    Create PatternMatching.java

---
+++ src/CS1061HW6/PatternMatching.java
@@ -0,0 +1,10 @@
+package CS1061HW6;
+
+public class PatternMatching {
+    public static void main(String[] args) {
+        String string1 = "string1";
+        String string2 = "string2";
+
+        
+    }
+}
