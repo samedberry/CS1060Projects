c818ebd8c40b08230a2a3eef2fadcaff1e1ea36d
samedberry <143440194+samedberry@users.noreply.github.com>
2024-03-19 13:03:32 -0600

    emptied Test

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,5 @@
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//
-//        //user entered string
-//        String text = "text";
-//
-//        Set vowels = new HashSet<Character>(5);
-//        vowels.add('a');
-//        vowels.add('e');
-//        vowels.add('i');
-//        vowels.add('o');
-//        vowels.add('u');
-//
-//        Character[] letters = text.toCharArray();
-//
-//    }
-//}
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
