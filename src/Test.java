f92717c82d5b0e929af345009f719e1a633d8b6b
samedberry <143440194+samedberry@users.noreply.github.com>
2024-03-26 13:49:53 -0600

    save URLs in Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,7 @@
+//URLs from class on 3/26
+//https://liveexample.pearsoncmg.com/liang/animation/animation.html
+//https://media.pearsoncmg.com/ph/esm/ecs_liang_ijp_11/cw/
+
 public class Test {
     public static void main(String[] args) {
 
